474ebbc9f108371e34c39155a1af6399893a1ed5
DRG Enterprises <dgoertz@accesscomm.ca>
2017-02-20 08:51:23 -0600

    Cleaned up code in BinaryStdin.

--- Fixed.playground/Sources/Data-Structures/BinaryStdIn.swift
+++ Fixed.playground/Sources/Data-Structures/BinaryStdIn.swift
@@ -1,28 +1,27 @@
-import Foundation
 
-enum IOErrors: Error
-{
-    case BadParameter(message: String)
-    case NothingFound
-}
+import UIKit
+import Foundation
+import PlaygroundSupport
+
+import Foundation
 
 public class BinaryStdIn
 {
-    var file:        URL
-    var fh:          FileHandle
-    var readAhead:   Int
+    var file:        URL!
+    var fh:          FileHandle!
+    var readAhead:   Int!
     var dataBuffer:  Data?
-    var bufferIndex: Int?
+    var byteIndex:   Int?
     var bitIndex:    Int?
     var endOfFile:   Bool = false
     
     public init(fileName: String, ext: String, readAhead: Int)
     {
         guard let path = Bundle.main.url(forResource: fileName, withExtension: ext)
-        else
+            else
         {
             print("Failed to locate the file \(fileName).\(ext) in the resources of this plsyground!")
-            abort()
+            return
         }
         var fh: FileHandle?
         do
@@ -36,9 +35,18 @@ public class BinaryStdIn
         self.file = path
         self.fh = fh!
         self.readAhead = readAhead
+        guard self.readNextDataBuffer() > 0 else {
+            self.endOfFile = true
+            abort()
+        }
     }
     
-    public func readNextBuffer() -> Int
+    public var atEndOfFile: Bool
+    {
+        return self.endOfFile
+    }
+    
+    public func readNextDataBuffer() -> Int
     {
         var dataBuffer = self.fh.readData(ofLength: self.readAhead)
         if dataBuffer.count == 0
@@ -47,57 +55,113 @@ public class BinaryStdIn
             return 0
         }
         self.dataBuffer = dataBuffer
-        self.bufferIndex = 0
+        self.byteIndex = 0
         self.bitIndex = 0
         self.endOfFile = false
         return dataBuffer.count
     }
     
-    public func readBoolean() -> Bool
+    public func incrementByteIndex() -> Void
     {
-        let currentByte = (self.dataBuffer?[self.bufferIndex!])! as UInt8
-        let bit = UInt8(self.bitIndex!) & currentByte
+        self.byteIndex! = self.byteIndex! + 1
+        self.bitIndex! = 0
+        if self.outOfData()
+        {
+            let _ = self.readNextDataBuffer()
+        }
+    }
+    
+    public func incrementBitIndex() -> Void
+    {
         self.bitIndex! = self.bitIndex! + 1
         if self.bitIndex! > 7
         {
-            self.bufferIndex! = self.bufferIndex! + 1
-            self.bitIndex! = 0
-            if self.bufferIndex! >= (self.dataBuffer?.count)!
-            {
-                let _ = self.readNextBuffer()
-            }
+            self.incrementByteIndex()
         }
-        return bit > 0 ? true : false
     }
     
-    public func readChar() -> UInt8
+    public func getCurrentByte() -> UInt8
     {
-        let currentByte = (self.dataBuffer?[self.bufferIndex!])! as UInt8
-        self.bufferIndex! = self.bufferIndex! + 1
-        self.bitIndex! = 0
-        if self.bufferIndex! >= (self.dataBuffer?.count)!
-        {
-            let _ = self.readNextBuffer()
-        }
-        return currentByte
+        let retVal = (self.dataBuffer?[self.byteIndex!])! as UInt8
+        return retVal
+    }
+    
+    public func createBitMaskFromBitIndex() -> UInt8
+    {
+        return (0x01 << UInt8(self.bitIndex!))
+    }
+    
+    public func outOfData() -> Bool
+    {
+        return (self.byteIndex! >= (self.dataBuffer?.count)!)
+    }
+    
+    public func readBoolean() -> Bool
+    {
+        let currentByte = self.getCurrentByte()
+        let mask = self.createBitMaskFromBitIndex()
+        let desiredBit = mask & currentByte
+        self.incrementBitIndex()
+        return desiredBit > 0 ? true : false
+    }
+    
+    public func readChar() -> CChar
+    {
+        let c: CChar = CChar(self.getCurrentByte())
+        self.incrementByteIndex()
+        return c
+    }
+    
+    public func readShort() -> UInt16
+    {
+        let highByte: UInt16 = UInt16(self.getCurrentByte())
+        self.incrementByteIndex()
+        let lowByte: UInt16 = UInt16(self.getCurrentByte())
+        self.incrementByteIndex()
+        return (highByte << 8) | lowByte
+    }
+    
+    public func readInt() -> UInt32
+    {
+        let firstByte: UInt32 = UInt32(self.getCurrentByte())
+        self.incrementByteIndex()
+        let secondByte: UInt32 = UInt32(self.getCurrentByte())
+        self.incrementByteIndex()
+        let thirdByte: UInt32 = UInt32(self.getCurrentByte())
+        self.incrementByteIndex()
+        let fourthByte: UInt32 = UInt32(self.getCurrentByte())
+        self.incrementByteIndex()
+        return UInt32(firstByte << 24) | UInt32(secondByte << 16) | UInt32(thirdByte << 8) | UInt32(fourthByte)
+    }
+    
+    public func readLong() -> UInt64
+    {
+        let firstByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let secondByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let thirdByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let fourthByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let fifthByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let sixthByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let seventhByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        let eighthByte: UInt64 = UInt64(self.getCurrentByte())
+        self.incrementByteIndex()
+        return UInt64(firstByte << 56) | UInt64(secondByte << 48) | UInt64(thirdByte << 40) | UInt64(fourthByte  << 32) | UInt64(fifthByte << 24) | UInt64(sixthByte << 16) | UInt64(seventhByte << 8) | UInt64(eighthByte)
     }
     
     public func close() -> Void
     {
         self.fh.synchronizeFile()
         self.fh.closeFile()
-        self.bufferIndex = nil
+        self.byteIndex = nil
         self.bitIndex = nil
         self.endOfFile = true
-    }
-    
-    // bitColumn starts at 0.
-    static func calcBytesNeeded(forBitColumn bitColumn: Int) -> Int
-    {
-        let valueNeeded = pow(2,bitColumn)
-        let numBytesNeeded: Int = (bitColumn / 8) + 1
-        let maxValue = pow(2,(numBytesNeeded * 8)) - 1
-        return (maxValue < valueNeeded) ? numBytesNeeded + 1 : numBytesNeeded
     }
     
     func readChar(numBits: Int) throws -> UInt16
@@ -110,3 +174,4 @@ public class BinaryStdIn
         return false
     }
 }
+
